00fc6a391635b786406fdbc801061b77edf9e02d
Nicki Nixon <nickitza@gmail.com>
2019-07-31 09:05:22 -0600

    add app.post

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var express = require('express')
 var app = express()
+var bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs");
+var friends = ["Sarah", "Nicki", "Eric", "Lily", "Samwise", "Miranda"]
 
 app.get('/', function(req, res){
   res.render('home')
 })
 
 app.get('/friends', function(req, res){
-  var friends = ["Sarah", "Nicki", "Eric", "Lily", "Samwise", "Miranda"]
   res.render("friends", {friends: friends})
 })
 
 app.post('/addfriend', function(req,res){
-  console.log(req.body)
-  res.send("Post Route")
+  var newFriend = req.body.name
+  friends.push(newFriend)
+  res.redirect("/friends")
 })
 
 app.listen(3000)
